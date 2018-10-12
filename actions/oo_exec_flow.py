db2f8e27f4762aa48723549f00494e1b6b32e753
sgottepulla <sgottepulla@csc.com>
2018-10-12 14:32:08 +0530

    Update actions/oo_exec_flow.py

--- actions/oo_exec_flow.py
+++ actions/oo_exec_flow.py
@@ -9,8 +9,8 @@ class OOFlowExecutionAction(Action):
          self.logger.info("OOFlowExecutionAction triggered "+ str(datetime.datetime.now()));
          self.logger.info(oo_flow_id)
          self.logger.info(oo_flow_name)
-         # r = requests.post('http://hpoo.demodxc.com:8080/oo/rest/v2/executions', auth=('svc_hpoo','ph00$Vc'), json={'flowUuid':'9307a65d-a4c2-4495-9bee-9c77413b6baa','runname':'Smart Store transaction failure for user'})
-         # print(r.status_code) # HTTP 201 is ok
-         # print(r.json()) # Prints the Run-ID
-         # return (True, r.json())  
-         return (True," OO Action "+ str(datetime.datetime.now()))
+         r = requests.post('http://hpoo.demodxc.com:8080/oo/rest/v2/executions', auth=('svc_hpoo','ph00$Vc'), json={'flowUuid':'9307a65d-a4c2-4495-9bee-9c77413b6baa','runname':'Smart Store transaction failure for user'})
+         #print(r.status_code) # HTTP 201 is ok
+         #print(r.json()) # Prints the Run-ID
+         return (True, r.json())  
+         # return (True," OO Action "+ str(datetime.datetime.now()))
