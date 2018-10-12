8debedef0eb51d7854bf8890bb2b0de9427c7759
sgottepulla <sgottepulla@csc.com>
2018-10-12 12:10:52 +0530

    commit

--- sensors/jira_sensor.py
+++ sensors/jira_sensor.py
@@ -7,13 +7,12 @@ class JiraOOSensor(PollingSensor):
     def setup(self):
         # Setup stuff goes here. For example, you might establish connections
         # to external system once and reuse it. This is called only once by the system.
-		self._jira_client = JIRA('http://jira-ams.demodxc.com',auth=('autoresolver', 'password'))
-        pass
+        self._jira_client = JIRA('http://jira-ams.demodxc.com',auth=('autoresolver', 'password'))
 
     def poll(self):
        	for issue in jira.search_issues('project = ATMTDEMO and created >= -1d and (description ~ sobhan or  description ~glsr)', maxResults=30):
-			self._dispatch_issues_trigger(issue)
-	    pass
+           self._dispatch_issues_trigger(issue)
+	    
 
     def cleanup(self):
         # This is called when the st2 system goes down. You can perform cleanup operations like
@@ -31,7 +30,7 @@ class JiraOOSensor(PollingSensor):
     def remove_trigger(self, trigger):
         # This method is called when trigger is deleted
         pass
-
+		
     def _dispatch_issues_trigger(self, issue):
         trigger = 'jira_oo.oo_issues_tracker'
         payload = {}
@@ -42,6 +41,6 @@ class JiraOOSensor(PollingSensor):
         payload['created'] = issue.raw['fields']['created']
         payload['assignee'] = issue.raw['fields']['assignee']
         payload['summary'] = issue.raw['fields']['summary']
-		payload['description'] = issue.raw['fields']['description']
+        payload['description'] = issue.raw['fields']['description']
         payload['issue_type'] = issue.raw['fields']['issuetype']['name']
         self._sensor_service.dispatch(trigger, payload)
