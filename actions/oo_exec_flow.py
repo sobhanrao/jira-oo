a5cbabd17d7ee319ee9a5b799dbf03b2982846f7
sgottepulla <sgottepulla@csc.com>
2018-10-12 13:58:43 +0530

    commit

--- actions/oo_exec_flow.py
+++ actions/oo_exec_flow.py
@@ -7,7 +7,7 @@ from st2common.runners.base_action import Action
 class OOFlowExecutionAction(Action):
     def run(self, flowid,flowname):
          self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
-         self._logger("OOFlowExecutionAction triggered "+datetime.datetime.now());
+         self._logger.debug("OOFlowExecutionAction triggered "+ str(datetime.datetime.now()));
          self._logger.debug(flowid)
          self._logger.debug(flowname)
          # r = requests.post('http://hpoo.demodxc.com:8080/oo/rest/v2/executions', auth=('svc_hpoo','ph00$Vc'), json={'flowUuid':'9307a65d-a4c2-4495-9bee-9c77413b6baa','runname':'Smart Store transaction failure for user'})
